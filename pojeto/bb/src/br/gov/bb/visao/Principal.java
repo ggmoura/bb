666451f96f82dc6a389dece851a192371a410ab4
Gleidson Guimarães Moura [instrutor] <gleidson.gmoura@gmail.com>
2016-07-15 21:37:27 -0300

    Implementação do método principal para execução do sistema

--- pojeto/bb/src/br/gov/bb/visao/Principal.java
+++ pojeto/bb/src/br/gov/bb/visao/Principal.java
@@ -1,5 +1,9 @@
 package br.gov.bb.visao;
 
 public class Principal {
-
+	public static void main(String[] args) {
+		TelaBB tela = new TelaBB();
+		tela.iniciarOperacao();
+		System.out.println("Obrigada, volte sempre!");
+	}
 }
