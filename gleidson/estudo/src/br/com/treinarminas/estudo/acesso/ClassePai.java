57b96a7543bb3f36cea78a5a4568d912cf477283
Gleidson Guimarães Moura [instrutor] <gleidson.gmoura@gmail.com>
2016-07-18 20:11:37 -0300

    modificador acesso

---
+++ gleidson/estudo/src/br/com/treinarminas/estudo/acesso/ClassePai.java
@@ -0,0 +1,9 @@
+package br.com.treinarminas.estudo.acesso;
+
+public class ClassePai {
+
+	String nome;
+	protected String sobrenome;
+	private String privado;
+	
+}
---
+++ gleidson/estudo/src/br/com/treinarminas/estudo/acesso/ModificadorProtected.java
@@ -0,0 +1,9 @@
+package br.com.treinarminas.estudo.acesso;
+
+public class ModificadorProtected extends ClassePai {
+
+	public void imprimir() {
+		System.out.println(sobrenome);
+	}
+	
+}
