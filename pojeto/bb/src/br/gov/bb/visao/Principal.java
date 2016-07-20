45184060a92b3ed7cb9d92aaaac709cb38638cbd
Gleidson Guimarães Moura [instrutor] <gleidson.gmoura@gmail.com>
2016-07-20 20:13:54 -0300

    constante

--- pojeto/bb/src/br/gov/bb/visao/Principal.java
+++ pojeto/bb/src/br/gov/bb/visao/Principal.java
@@ -1,7 +1,11 @@
 package br.gov.bb.visao;
 
+import br.gov.bb.modelo.banco.IConstante;
+
 public class Principal {
 	public static void main(String[] args) {
+		System.out.println(IConstante.IMensagem.MENSAGEM_BOAS_VINDAS);
+		
 		TelaBB tela = new TelaBB();
 		tela.iniciarOperacao();
 		System.out.println("Obrigada, volte sempre!");
