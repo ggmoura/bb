5f686ea311d5769fd6df14677bb11ee66dbc10c0
ADM <ADM@TRE-BHZ-068>
2016-07-21 22:42:31 -0300

    Enum

---
+++ Isabela/Locadora/src/AtividadeEnum.java
@@ -0,0 +1,15 @@
+public class AtividadeEnum {
+
+	public static void main(String[] args) {
+
+		DiaSemana[] dias = DiaSemana.values();
+
+		int numeroDia = 6;
+		
+		DiaSemana dia = dias[numeroDia];
+		
+		System.out.print("Sábado: " + dia);
+
+	}
+
+}
