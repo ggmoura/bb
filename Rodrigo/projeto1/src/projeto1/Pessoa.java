9d469464d8627d8ad2b8f38b837648b5a6d7d6af
Rodrigo <Rodrigo@Rodrigo>
2016-07-22 19:48:29 -0300

    Criando classe pessoa

---
+++ Rodrigo/projeto1/src/projeto1/Pessoa.java
@@ -0,0 +1,18 @@
+package projeto1;
+
+public class Pessoa {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void correr(){
+		System.out.println("Estou correndo");
+	
+	}
+
+}
