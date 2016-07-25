4cc53b8b24ce03cc6f6a55b37207e8981cbfeb6d
ADM <ADM@TRE-BHZ-068>
2016-07-25 19:43:16 -0300

    Map

--- Isabela/Locadora/src/ImprimirNomes.java
+++ Isabela/Locadora/src/ImprimirNomes.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ImprimirNomes {
@@ -7,21 +9,34 @@ public class ImprimirNomes {
 	public static void main(String[] args) {
 
 		List<String> nomes = new ArrayList<>();
+		Map<Character, List<String>> mapNomes = new HashMap();
 
 		Scanner leitura = new Scanner(System.in);
 
 		String nome = null;
 
 		do {
-
 			System.out.print("Informe o nome ou sair: ");
 			nome = leitura.nextLine();
 			if (!nome.equals("sair")) {
 				nomes.add(nome);
 			}
-		} while (!nome.equals("sair"));
-		
+		} while (!nome.equalsIgnoreCase("sair"));
+
+		leitura.close();
 		System.out.println(nomes);
-		
+		Character primeiraLetra = null;
+		List<String> value = null;
+
+		for (String nomeTemp : nomes) {
+			primeiraLetra = nomeTemp.charAt(0);
+			if (mapNomes.containsKey(primeiraLetra)) {
+				value = new ArrayList<String>();
+				mapNomes.put(primeiraLetra, value);
+			}
+			mapNomes.get(primeiraLetra).add(nomeTemp);
+		}
+		System.out.println(mapNomes);
+
 	}
 }
