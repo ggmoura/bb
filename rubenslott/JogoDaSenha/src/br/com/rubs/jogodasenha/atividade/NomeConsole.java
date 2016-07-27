8bf51ca582a321ba38327c7e48ef971b37c14fb0
ADM <ADM@SL09MICRO91>
2016-07-27 19:08:30 -0300

    Atividade

---
+++ rubenslott/JogoDaSenha/src/br/com/rubs/jogodasenha/atividade/NomeConsole.java
@@ -0,0 +1,11 @@
+package br.com.rubs.jogodasenha.atividade;
+
+import javax.swing.JOptionPane;
+
+public class NomeConsole {
+
+	public static void main(String[] args) {
+		System.out.println("Rubens Lott de Almeida Cunha");
+		//JOptionPane.showMessageDialog(null, "Rubens Lott de Almeida Cunha");
+	}
+}
