1ee7684294e965adead502f7db0fe06c90501d1c
ADM <ADM@SL09MICRO91>
2016-07-27 22:30:42 -0300

    x

--- rubenslott/JogoDaSenha/src/br/com/rubs/jogodasenha/jdbc/TesteJDBC.java
+++ rubenslott/JogoDaSenha/src/br/com/rubs/jogodasenha/jdbc/TesteJDBC.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class TesteJDBC {
 
 	public static void main(String[] args) throws SQLException {
-		Connection conexao = ConnectionFactory().getInstance().getConnection();
+		Connection conexao = ConnectionFactory.getInstance().getConnection();
 		//Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost/treinar", "root", "");
 		System.out.println("Conectado!");
 		conexao.close();
