12c5265dcf5d431d87c32bd2dbbf65dec10b7a1d
ADM <ADM@SL04MICRO85>
2016-08-04 22:49:44 -0300

    e

---
+++ ricardomatos/agenda-trn-ricardo/src/br/com/treinarminas/agenda/controller/ServletController.java
@@ -0,0 +1,32 @@
+package br.com.treinarminas.agenda.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletController
+ */
+@WebServlet("*.trn")
+public class ServletController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("texto");
+	}
+
+}
